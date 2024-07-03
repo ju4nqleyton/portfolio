e0b7143ecfa41bc8312defff6856cf069b735c50
ju4nqleyton <ju4nqleyton@gmail.com>
2024-07-02 21:44:57 -0500

    custom domain

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <AnimatePresence>
       <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
-        <Router basename="portfolio/">
+        <Router basename="/">
           <ScrollToTop />
           <AppHeader />
           <Suspense fallback={''}>
